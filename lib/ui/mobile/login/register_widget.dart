3d171cfed3728ab567f8a95a22e0e2af37093db4
JustinW <youdontneedtoknowmyemail@google.com>
2023-08-24 16:12:38 +0100

    Update login and register UI

--- lib/ui/mobile/login/register_widget.dart
+++ lib/ui/mobile/login/register_widget.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:c_hat/dev/ext/widget_ext.dart';
 import 'package:c_hat/ui/mobile/login/verify_widget.dart';
 import 'package:c_hat/ui/shared/heading_text.dart';
 import 'package:c_hat/ui/shared/register_bloc/register_bloc.dart';
@@ -7,6 +10,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:platform_ui/platform_ui.dart';
+
+import '../splash_screen.dart';
 
 class RegisterRoute extends StatefulWidget {
   final Platform platform;
@@ -24,11 +29,18 @@ class _RegisterRouteState extends State<RegisterRoute> {
   final TextEditingController _passwordTextFieldController =
       TextEditingController();
 
-  final TextEditingController _mailTextFieldController =
+  final TextEditingController _mailIdTextFieldController =
       TextEditingController();
 
   final TextEditingController _serverIpTextFieldController =
       TextEditingController();
+
+  var _ipError = false;
+  var _mailError = false;
+  var _passwordError = false;
+  var _usernameError = false;
+
+  final errorText = "This cannot be empty";
 
   @override
   Widget build(BuildContext context) {
@@ -39,107 +51,151 @@ class _RegisterRouteState extends State<RegisterRoute> {
         cupertinoBar: const CupertinoNavigationBar(
           middle: Text("Welcome!"),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(height: 128),
-              const HeadingText("Register"),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: PlatformTextField(widget.platform,
-                    hint: "Server IP",
-                    controller: _serverIpTextFieldController),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: PlatformTextField(widget.platform,
-                    hint: "Username", controller: _usernameTextFieldController),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: PlatformTextField(widget.platform,
-                    hint: "Password", controller: _passwordTextFieldController),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: PlatformTextField(widget.platform,
-                    hint: "Email", controller: _mailTextFieldController),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+        body: Center(
+          child: SingleChildScrollView(
+            child: SizedBox(
+              width: 300,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: PlatformFilledButton(widget.platform, onPressed: () {
-
-                      final bloc = RegisterBloc()
-                        ..add(RegisterSentEvent(
-                          mailId: _mailTextFieldController.text,
-                          username: _usernameTextFieldController.text,
-                          password: _passwordTextFieldController.text,
-                          wsUrl: _serverIpTextFieldController.text
-                        ));
-
-                      showDialog(
-                          context: context,
-                          builder: (context) {
-                            return BlocConsumer<RegisterBloc, RegisterState>(
-                                bloc: bloc,
-                                builder: ((context, state) {
-                                  if (state is RegisterWait) {
-                                    return const SizedBox(
-                                      height: 50,
-                                      child: AlertDialog(
-                                        content: Center(
-                                          child: SizedBox(
-                                            height: 100,
-                                            child: CircularProgressIndicator(),
-                                          ),
-                                        ),
-                                      ),
-                                    );
-                                  } else if (state is RegisterFailed) {
-                                    return SizedBox(
-                                      height: 100,
-                                      child: AlertDialog(
-                                        content: Center(
-                                          child: SizedBox(
-                                            height: 100,
-                                            child: Text(
-                                              state.message,
-                                              style:
-                                                  const TextStyle(fontSize: 32),
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    );
-                                  } else {
-                                    return Container();
-                                  }
-                                }),
-                                listener: ((context, state) {
-                                  if (state is RegisterVerify) {
-                                    Navigator.push(context,
-                                        MaterialPageRoute(builder: ((context) {
-                                      return VerifyWidget(
-                                          widget.platform, bloc);
-                                    })));
-                                  }
-                                }));
-                          });
-                    }, child: const Text("Register")),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: PlatformButton(widget.platform, onPressed: () {
-                      //SystemNavigator.pop();
-                    }, child: const Text("Back")),
-                  )
+                  const HeadingText("Register"),
+                  PlatformTextField(widget.platform,
+                          hint: "Server IP",
+                          controller: _serverIpTextFieldController,
+                          errorText: _ipError ? errorText : null)
+                      .withPadding(),
+                  PlatformTextField(
+                    widget.platform,
+                    hint: "Username",
+                    controller: _usernameTextFieldController,
+                    errorText: _usernameError ? errorText : null,
+                  ).withPadding(),
+                  PlatformTextField(widget.platform,
+                          hint: "Password",
+                          controller: _passwordTextFieldController,
+                          errorText: _passwordError ? errorText : null)
+                      .withPadding(),
+                  PlatformTextField(widget.platform,
+                          hint: "Email",
+                          controller: _mailIdTextFieldController,
+                          errorText: _mailError ? errorText : null)
+                      .withPadding(),
+                  PlatformFilledButton(widget.platform,
+                  minimumWidth: 256,
+                          onPressed: onRegisterButtonPressed,
+                          child: const Text("Register"))
+                      .withPadding()
                 ],
               ),
-            ],
+            ),
           ),
         ));
+  }
+
+  void onRegisterButtonPressed() {
+    if (_serverIpTextFieldController.text.isEmpty ||
+        _mailIdTextFieldController.text.isEmpty ||
+        _passwordTextFieldController.text.isEmpty ||
+        _usernameTextFieldController.text.isEmpty) {
+      if (_passwordTextFieldController.text.isEmpty) {
+        setState(() {
+          _passwordError = true;
+        });
+      } else {
+        setState(() {
+          _passwordError = false;
+        });
+      }
+
+      if (_mailIdTextFieldController.text.isEmpty) {
+        setState(() {
+          _mailError = true;
+        });
+      } else {
+        setState(() {
+          _mailError = false;
+        });
+      }
+      if (_serverIpTextFieldController.text.isEmpty) {
+        setState(() {
+          _ipError = true;
+        });
+      } else {
+        setState(() {
+          _ipError = false;
+        });
+      }
+      if (_usernameTextFieldController.text.isEmpty) {
+        setState(() {
+          _usernameError = true;
+        });
+      } else {
+        _usernameError = false;
+      }
+      return;
+    }
+
+    final bloc = RegisterBloc()
+      ..add(RegisterSentEvent(
+          mailId: _mailIdTextFieldController.text,
+          username: _usernameTextFieldController.text,
+          password: _passwordTextFieldController.text,
+          wsUrl: _serverIpTextFieldController.text));
+
+    showDialog(
+        context: context,
+        builder: (context) {
+          return BlocConsumer<RegisterBloc, RegisterState>(
+              bloc: bloc,
+              builder: ((context, state) {
+                if (state is RegisterWait) {
+                  return SizedBox(
+                      height: 50,
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                        child: AlertDialog(
+                          content: SizedBox(
+                            height: 100,
+                            width: 100,
+                            child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  const SplashScreen().withPadding(),
+                                  const CircularProgressIndicator()
+                                      .withPadding()
+                                ]),
+                          ),
+                        ),
+                      ));
+                } else if (state is RegisterFailed) {
+                  return SizedBox(
+                    height: 100,
+                    child: AlertDialog(
+                      content: Center(
+                        child: SizedBox(
+                          height: 100,
+                          child: Text(
+                            state.message,
+                            style: const TextStyle(fontSize: 32),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                } else {
+                  return Container();
+                }
+              }),
+              listener: ((context, state) {
+                if (state is RegisterVerify) {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: ((context) {
+                    return VerifyWidget(widget.platform, bloc);
+                  })));
+                }
+              }));
+        });
   }
 }
